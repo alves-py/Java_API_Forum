84d4ffc98095eaa4766224ee66f7f632bf8d3360
Felipe Alves <felipe.daconceicao@jala.university>
2024-04-15 23:03:07 -0300

    chore[CSI]: initial commit

---
+++ src/main/java/com/example/csiauth/CsiAuthApplication.java
@@ -0,0 +1,13 @@
+package com.example.csiauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsiAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CsiAuthApplication.class, args);
+    }
+
+}
